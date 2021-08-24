674588e051ae1d029fca79e83b55febc94cda95c
tcollincox <tcollincox@gmail.com>
2021-08-23 21:35:47 -0400

    opportunity display and match with volunteers

--- src/components/OpportunityDisplayAndFilter.vue
+++ src/components/OpportunityDisplayAndFilter.vue
@@ -1,15 +1,142 @@
 <template>
+<div id="page">
+  <editAndDelete v-if="pageDisplay=='editAndDelete'" v-bind:opportunity="selectedOpportunity" v-on:change-to-display="changeToDisplay"/>
+  <matchWithVolunteer v-if="pageDisplay=='matchWithVolunteer'" v-bind:opportunity="selectedOpportunity" v-on:change-to-display="changeToDisplay"/>
+  <div v-if="pageDisplay=='display'">
     <div>
-        <h1>Display</h1>
+      <h3>Display And Filter</h3>
+      <b-dropdown id="filter-dropdown" text="Choose Filter" class="m-md-2">
+        <b-dropdown-item @click="filterByDate('Most Recent')">Most Recent</b-dropdown-item>
+        <b-dropdown-item @click="filterByCenter('By Center')">By Center</b-dropdown-item>
+      </b-dropdown>
     </div>
+    <table v-if="opportunitiesFiltered.length>0" class="table-display">
+      <tr>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Skills</th>
+        <th>Center</th>
+        <th>Maximum Number of People</th>
+        <th>Date</th>
+        <th>Edit</th>
+        <th>Match</th>
+      </tr>
+      <tr v-for="(opportunity, i) in opportunitiesFiltered" :key="opportunity.id" >
+        <td>{{opportunity.title}}</td>
+        <td>{{opportunity.description}}</td>
+        <td>{{opportunity.skills}}</td>
+        <td>{{opportunity.center}}</td>
+        <td>{{opportunity.maxPeople}}</td>
+        <td>{{opportunity.date}}</td>
+        <td class="table-click" @click="edit(i)"> Edit </td>
+        <td class="table-click" @click="matchWithVolunteer(i)">Match</td>
+      </tr>
+    </table>
+    <p v-if="opportunitiesFiltered.length == 0">No results to display</p>
+  </div>
+</div>
 </template>
 
 <script>
+import editAndDelete from '../components/OpportunityEditandDelete.vue'
+import matchWithVolunteer from '../components/OpportunityFindVolunteer.vue'
+
 export default {
-  
+
+  data(){
+    return{
+      opportunities:[],
+      opportunitiesFiltered:[],
+      filter: "all",
+      fields:["title", "description", "skills", "center", "maxPeople", "date"],
+      pageDisplay: "display",
+      selectedOpportunity: null,
+      }
+  },
+  mounted(){
+    this.getOpportunities()
+  },
+  methods:{
+    getOpportunities(){
+      this.axios.get('http://localhost:3001/opportunities')
+      .then(response => (this.opportunities = response.data, this.opportunitiesFiltered=response.data ))
+      .catch(error => console.log(error));
+    },
+    //check this out for date filter logic
+    filterByDate(){
+      this.opportunitiesFiltered = [];
+      for(var i = 0; i < this.opportunities.length; i++){
+        for(var j = 0; j < this.opportunity.date.length; j++){
+          if(this.opportunities[i].date == this.opportunity.date[j]){
+          this.opportunitiesFiltered.push(this.opportunities[i]);
+          }
+        }
+      }
+    },
+    
+    //check this out for center filter logic
+    filterByCenter(){
+      this.opportunitiesFiltered = [];
+      for(var i = 0; i < this.opportunities.length; i++){
+        for(var j = 0; j < this.opportunity.center.length; j++){
+          if(this.opportunities[i].center == this.opportunity.center[j]){
+          this.opportunitiesFiltered.push(this.opportunities[i]);
+          }
+        }
+      }
+    },
+    
+    returnAll(){
+      this.opportunitiesFiltered = [];
+      this.opportunitiesFiltered = this.opportunities;
+    },
+    edit(opportunity){
+      this.selectedOpportunity = this.opportunitiesFiltered[opportunity];
+      this.pageDisplay = 'editAndDelete';
+    },
+    changeToDisplay(){
+      this.selectedOppotunity = null;
+      this.getOpportunities();
+      this.returnAll();
+      this.pageDisplay = "display";
+    },
+    deleteOpportunity(opportunity){
+      this.selectedOpportunity = this.opportunitiesFiltered[opportunity];
+      let conformation = confirm(this.selectedOpportunity.title + " " + this.selectedOppotunity.center + " is going to be permanently deleted");
+      if(conformation){
+        let path = 'http://localhost:3001/opportunities/Delete/' + this.selectedOppotunity._id;
+        this.axios.delete(path);
+        this.opportunitiesFiltered.splice(opportunity,1);
+      }
+    },
+    matchWithVolunteer(opportunityIndex){
+      this.selectedOpportunity = this.opportunitiesFiltered[opportunityIndex];
+      this.pageDisplay = 'matchWithVolunteer';
+    }
+  },
+  components:{
+    editAndDelete,
+    matchWithVolunteer
+  }
 }
 </script>
 
 <style scoped>
+  .table-display{
+    margin-left: auto;
+    margin-right: auto;
+    width:50%;
+  }
+  table, th, td {
+  border: 1px solid black;
+  }
 
+  table{
+    border-collapse: collapse;
+  }
+
+  .table-click:hover{
+    cursor:pointer;
+    color:red;
+  }
 </style>
--- src/components/OpportunityFindVolunteer.vue
+++ src/components/OpportunityFindVolunteer.vue
@@ -1,15 +1,85 @@
 <template>
-    <div>
-        <h1>Find</h1>
-    </div>
+  <div>
+    <h3>Find Volunteer for {{opportunity.title}}</h3>
+    <h4 v-if="displayCenter">{{opportunity.center}}</h4>
+    <h4 v-if="displaySkills">{{opportunity.skills}}</h4>
+    <b-button @click="$emit('change-to-display')">Back to display</b-button>
+    <b-dropdown id="filter-dropdown" text="Choose Filter" class="m-md-2">
+        <b-dropdown-item @click="returnAll()">All</b-dropdown-item>
+      </b-dropdown>
+    <p v-for="(volunteer, i) in volunteersFiltered" :key="volunteer.id">
+      <b>First Name:</b> {{volunteer.firstName}} |<b> Last Name:</b> {{volunteer.lastName}} <span v-if="displayCenter">|<b> Centers:</b> {{volunteer.preferredCenters}}</span> <span v-if="displaySkills">|<b> Skills:</b> {{volunteer.skills}}</span> | <span @click="match(i), $emit('change-to-display')" class="clickable">Match</span>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  props:[
+    'opportunity'
+  ],
+  data(){
+    return{
+      volunteers: [],
+      volunteersFiltered:[],
+      filledOpportunity:{
+        title: this.opportunity.title,
+        center: this.opportunity.center,
+        opportunityId: this.opportunity._id,
+        volunteerId: "",
+        volunteerFirstName: "",
+        volunteerLastName: "",
+      },
+      displaySkills: false,
+      displayCenter : false
+    }
+  },
+  mounted(){
+    this.getVolunteers();
+  },
+  methods:{
+    getVolunteers(){
+      this.axios.get('http://localhost:3001/volunteers')
+      .then(response => (this.volunteers = response.data, this.volunteersFiltered = response.data))
+      .catch(error => console.log(error));
+    },
+    match(volunteersIndex){
+      let volunteer = this.volunteers[volunteersIndex];
+      this.filledOpportunity.volunteerFirstName = volunteer.firstName;
+      this.filledOpportunity.volunteerLastName = volunteer.lastName;
+      this.filledOpportunity.volunteerId = volunteer._id;
+      this.axios.post('http://localhost:3001/filledOpportunities/Post', {
+        title: this.filledOpportunity.title ,
+        center: this.filledOpportunity.center,
+        opportunityId: this.filledOpportunity.opportunityId,
+        volunteerId: this.filledOpportunity.volunteerId,
+        volunteerFirstName: this.filledOpportunity.volunteerFirstName,
+        volunteerLastName: this.filledOpportunity.volunteerLastName,
+        });
+      },
+      returnAll(){
+        this.volunteersFiltered = this.volunteers;
+        this.displaySkills = false;
+        this.displayCenter = false;
+      },
+    }
 }
 </script>
 
 <style scoped>
-
+button{
+  min-width: 80px;
+  margin: 4vh 2vw 4vh 2vw;
+  background-color:#FE7A53;
+  border:none;
+  width: 150px;
+  border-radius: 20px;
+}
+.clickable:hover{
+  cursor: pointer;
+  color: red;
+}
+li {
+  list-style: none;
+}
 </style>
